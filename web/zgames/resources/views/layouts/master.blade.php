934a02aa987afd8492cc03d221ae505e6fa431b0
César Benavente <cesar.dragonblanco@gmail.com>
2021-06-10 20:32:20 -0300

    pestañas nuevas

--- web/zgames/resources/views/layouts/master.blade.php
+++ web/zgames/resources/views/layouts/master.blade.php
@@ -4,17 +4,21 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
 
     <!-- Bootstrap CSS -->  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
-        <title>Hello, world!</title>
+        <title>Nierda</title>
 </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <nav class="navbar navbar-expand-lg navbar-light bg-success">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="{{route('home')}}">Z Games</a>
+                    <a class="navbar-brand" href="{{route('home')}}">
+                        <img class="logo" src="{{asset('img/logo.png')}}"/>
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                          <span class="navbar-toggler-icon"></span>
                     </button>
@@ -32,5 +36,8 @@
         <main class="container-fliud">
             @yield("contenido")
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script> src="{{asset('js/axion_config.js')}}"</script>
     </body>
 </html>
