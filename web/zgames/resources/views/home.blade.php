95bf37cef521617a765f2fc7e152682d809f1e0f
César Benavente <cesar.dragonblanco@gmail.com>
2021-06-16 01:17:31 -0300

    Generar base de la api rest agregando controller

--- web/zgames/resources/views/home.blade.php
+++ web/zgames/resources/views/home.blade.php
@@ -7,17 +7,14 @@
                 <div class="card-header bg-warning text-white">
                     <span>Agregar Consola</span>
                 </div>
+                <div class="card-body">
                 <div class="mb-3">
                 <label for="nombre-txt" class="form-label">Nombre</label>
                 <input type="text" id="nombre-txt" class="form-control">
                 </div>
                 <div class="mb-3">
                     <label for="marca-select" class="form-label">Marca</label>
-                    <select class="form-select" id="marca-select">
-                        <option value="microsoft">Microsoft</option>
-                        <option value="sony">Sony</option>
-                        <option value="nintendo">Nintendo</option>
-                        <option value="sega">Sega</option>
+                    <select class="form-select" id="marca-select">     
                     </select>
                 </div>
                 <div class="mb-3">
@@ -25,9 +22,13 @@
                 <input type="number" class="form-control" id="anio-txt">
                 </div>
             </div>
+        </div>
             <div class="card-footer d-grid gap-1">
                 <button class="btn btn-info">Registrar</button>
             </div>
         </div>
     </div>
+@endsection
+@section("javascript")
+<script src="{{asset('js/home.js')}}"></script>
 @endsection
